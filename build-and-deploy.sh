0f282abef1eb8139753ddfe97c48f7a9873fd7f3
James Meyers <james.meyers@emberex.com>
2022-03-22 22:07:23 -0700

    Print expo token.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,5 +3,6 @@
 # See: https://github.com/expo/eas-cli
 echo "TODO: Build and deploy with the EAS CLI"
 aws sts get-caller-identity
-aws secretsmanager get-secret-value --secret-id expo-token
+echo "EXPO_TOKEN = $EXPO_TOKEN"
+# aws secretsmanager get-secret-value --secret-id expo-token
 # eas build -p all --non-interactive
