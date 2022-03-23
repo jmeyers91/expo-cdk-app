00c0c19cd9bcc399e1657670ebf827b5ca55543a
James Meyers <james.meyers@emberex.com>
2022-03-22 21:58:25 -0700

    Attempt to get expo-token secret from codebuild.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,3 +3,5 @@
 # See: https://github.com/expo/eas-cli
 echo "TODO: Build and deploy with the EAS CLI"
 aws sts get-caller-identity
+aws secretsmanager get-secret-value --secret-id expo-token
+# eas build -p all --non-interactive
