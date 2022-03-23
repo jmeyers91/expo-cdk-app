f3b378081ede40d70424b073afb6705db9fe3556
James Meyers <james.meyers@emberex.com>
2022-03-22 22:23:57 -0700

    update

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,4 +5,4 @@ echo "TODO: Build and deploy with the EAS CLI"
 aws sts get-caller-identity
 echo "EXPO_TOKEN = $EXPO_TOKEN"
 # aws secretsmanager get-secret-value --secret-id expo-token
-# eas build -p all --non-interactive
+eas build -p android --non-interactive
