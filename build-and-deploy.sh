0da38ae4e62364d715329188f196521cabfdd668
James Meyers <james.meyers@emberex.com>
2022-03-22 23:03:49 -0700

    Remove unnecessary commands from build script.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 # See: https://github.com/expo/eas-cli
-echo "TODO: Build and deploy with the EAS CLI"
-aws sts get-caller-identity
-echo "EXPO_TOKEN = $EXPO_TOKEN"
-# aws secretsmanager get-secret-value --secret-id expo-token
 eas build -p android --non-interactive
