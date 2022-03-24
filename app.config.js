664653adda0fab4560baa8bf32a2d2d6ca7a2d30
James Meyers <james.meyers@emberex.com>
2022-03-24 15:57:38 -0700

    More logging.

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default ({ config }) => {
   const buildCommitId = process.env.CODEBUILD_RESOLVED_SOURCE_VERSION ?? "";
-  console.log("Building commit", buildCommitId);
+  console.log("Building commit", buildCommitId, config);
   return {
     ...config,
     extra: {
