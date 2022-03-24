5575b7ec0dd57c2ece4ff4ac988293b2718fea9a
James Meyers <james.meyers@emberex.com>
2022-03-24 16:00:26 -0700

    Fix pre-build script.

--- pre-build.js
+++ pre-build.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
-const easConfig = require("./eas.json");
+const path = require("path");
+
+const easConfigPath = path.resolve(__dirname, "eas.json");
+const easConfig = JSON.parse(fs.readFileSync(easConfigPath, "utf-8"));
+const injectedEnv = {
+  CODEBUILD_RESOLVED_SOURCE_VERSION:
+    process.env.CODEBUILD_RESOLVED_SOURCE_VERSION,
+};
 
 const updatedEasConfig = {
   ...easConfig,
@@ -8,14 +15,12 @@ const updatedEasConfig = {
       key,
       {
         ...value,
-        env: {
-          ...value.env,
-          CODEBUILD_RESOLVED_SOURCE_VERSION:
-            process.env.CODEBUILD_RESOLVED_SOURCE_VERSION,
-        },
+        env: { ...value.env, ...injectedEnv },
       },
     ])
   ),
 };
 
-fs.writeFileSync("./app.json", JSON.stringify(updatedEasConfig, null, 2));
+fs.writeFileSync("./eas.json", JSON.stringify(updatedEasConfig, null, 2));
+
+console.log(`Injected Codebuild env into EAS env`, injectedEnv);
